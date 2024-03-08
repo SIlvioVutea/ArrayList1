fe448bc2c51da39cbd3a64ffa4e7928b9feb2f17
SIlvioVutea <silviovutea@gmail.com>
2024-03-08 12:14:56 +0100

    esercizio

--- src/Student.java
+++ src/Student.java
@@ -1,9 +1,10 @@
 public class Student {
     private String name;
     private int age;
-    public Student (String name,int age){
-        this.name=name;
-        this.age=age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
     public int getAge() {
